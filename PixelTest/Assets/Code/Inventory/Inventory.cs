3dfd6527a0fe80ffc264084ccd44b2029ba2312a
PatrykTlatlik <91884721+PatrykTlatlik@users.noreply.github.com>
2024-04-15 20:24:48 +0200

    Inventory test
    Dodanie testowego inventory

---
+++ PixelTest/Assets/Code/Inventory/Inventory.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance;
+    public List<Item> Items = new List<Item>();
+    public GameObject inventoryUI;
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("Item/ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("Item/ItemName").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
+    }
+}
